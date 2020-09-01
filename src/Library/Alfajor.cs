1c9075845386c0668d6b14e737be83266461a6fd
FabioMario <67033535+FabioMario@users.noreply.github.com>
2020-09-01 19:45:20 -0300

    Comentarios Alfajor Kiosko

---
+++ src/Library/Alfajor.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Expert_SRP
+{
+    public class Alfajor 
+    // No cumple con SRP porque tiene dos responsabilidades, la de decir si hay suficiente dinero para comprar un alfajor y la de convertir ese dinero a pesos.
+    {
+        public Alfajor(double precioMasa, double precioDulce)
+        {
+            this.PrecioDulce = precioDulce;
+            this.PrecioMasa = precioMasa;
+        }
+        public Double PrecioDulce {get;set;}
+        public Double PrecioMasa {get; set;}
+    }
+}
---
+++ src/Library/Kiosco.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Expert_SRP
+{
+    public class Kiosco 
+    // No cumple con SRP porque  no solo dice si puedes comprar o no, si no que tambien hace la conversion a pesos esa misma clase
+    {
+        public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
+        {
+            Double pesos = ConvertirAPesos(dinero, moneda);
+            return pesos >= a.PrecioDulce + a.PrecioMasa;
+        }
+        private Double ConvertirAPesos(Double dinero, String moneda)
+        {
+            if (moneda.Equals("U$S")) 
+            {
+                return dinero / 30;
+            } 
+            else if (moneda.Equals("$")) 
+            {
+                return dinero;
+            }
+            else 
+            {
+                return -1;
+            }
+        }
+    }
+}
